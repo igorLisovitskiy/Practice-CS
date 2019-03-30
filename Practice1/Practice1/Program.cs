a8a57bf5bc62ceebafb22aa06c47c4a785018b40
igorLisovitskyi <igorlisovitskiy@gmail.com>
2019-03-30 14:50:46 +0200

    Tested casting.

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -8,8 +8,16 @@ namespace Practice1
 {
     class Program
     {
+        static int Sum(long x, int y)
+        {
+            return (int) (x + y);
+        }
+
         static void Main(string[] args)
         {
+            long n1 = 32301; // implicit
+            short n2 = 15;
+            Console.WriteLine($"{n1} + {n2} = {Program.Sum(n1, n2)}");
         }
     }
 }
