b5552613bdfef3c3e65f2142dde69ed1d6bc9695
igorLisovitskyi <igorlisovitskiy@gmail.com>
2019-03-30 12:56:22 +0200

    Calculates area of a circle.

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace Practice1
 {
     class Program
     {
+        public static double CalculateArea(int radius)
+        {
+            const double pi = 3.14;
+            return pi * radius * radius;
+        }
+
         static void Main(string[] args)
         {
+            Write("Enter a radius: ");
+            string radiusStr = ReadLine();
+            int.TryParse(radiusStr, out int radius);
+            WriteLine($"The area of circle is: {Program.CalculateArea(radius)}");
         }
     }
 }
