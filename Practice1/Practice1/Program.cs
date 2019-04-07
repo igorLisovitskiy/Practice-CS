ec98015aa1a0860a7b774444bcfa3993c15cde9a
Igor Lisovitskiy <lisovitskiyigor@gmail.com>
2019-04-07 13:50:52 +0300

    Fixed ReadLine invocation.

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -14,7 +14,7 @@ namespace Practice1
         {
 
             Console.Write("Enter a radius: ");
-            string radiusStr = ReadLine();
+            string radiusStr = Console.ReadLine();
             int.TryParse(radiusStr, out int radius);
             Console.WriteLine($"The area of circle is: {Program.CalculateArea(radius)}");
 
@@ -28,8 +28,9 @@ namespace Practice1
             //check presicion
             const double Pi = Math.PI;
             double eNum = Math.E;
-            Console.WriteLine(Pi); // 3.14159265358979
-            Console.WriteLine(eNum); // 2.71828182845905
+            //Console.WriteLine(Pi); // 3.14159265358979
+            //Console.WriteLine(eNum); // 2.71828182845905
+
             //Control characters
             string newLine = "\nmystring 1";
             string tab = "\tmystring 2";
